9290f406a0405b33a4b68cc655dede0428005248
aaajii <jwpbanawan@gmail.com>
2022-08-05 02:58:38 +0800

    Set up sample data and home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Nav, Button } from "react-bootstrap";
+
+import CustomModal from "./components/CustomModal";
+import Content from "./components/Content";
+
+import "./App.css";
+
+const SAMPLE_DATA = [
+  {
+    id: "253",
+    title: "A3 Creating for T1 (cloned from 192)",
+    facilityAmount: 930000000,
+    facilityType: "TLF",
+    maturityDate: new Date(Date.now()).toLocaleDateString(),
+    margin: "LIBOR 0.0% p.a.",
+  },
+  {
+    id: "354",
+    title: "ST - Syndication something here",
+    facilityAmount: 930000000,
+    facilityType: "TLF",
+    maturityDate: new Date(Date.now()).toLocaleDateString(),
+    margin: "LIBOR 0.0% p.a.",
+  },
+];
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Privex Homework</p>
+        <Button onClick={() => setShowModal(true)}>Homework 1</Button>
       </header>
+
+      <CustomModal showModal={showModal} onHide={() => setShowModal(false)}>
+        <div className="w-100 h-100">
+          <Nav
+            variant="tabs"
+            defaultActiveKey="0"
+            onSelect={(selectedKey) => setSelectedIndex(selectedKey)}
+          >
+            {SAMPLE_DATA.map((data, i) => {
+              const { id, title } = data;
+              return (
+                <Nav.Item>
+                  <Nav.Link eventKey={i}>
+                    <h6 className="text-truncate fw-semibold">
+                      {id} - {title}
+                    </h6>
+                  </Nav.Link>
+                </Nav.Item>
+              );
+            })}
+          </Nav>
+          <Content data={SAMPLE_DATA[selectedIndex]} />
+        </div>
+      </CustomModal>
     </div>
   );
 }
