e23b196604aa2e2945adae4c7182044a984a83c1
aaajii <jwpbanawan@gmail.com>
2022-08-05 03:00:00 +0800

    Add CustomModal component

---
+++ src/components/CustomModal/index.js
@@ -0,0 +1,17 @@
+import { Modal } from "react-bootstrap";
+import "./index.css";
+
+function CustomModal({ showModal, onHide, children }) {
+  return (
+    <Modal
+      show={showModal}
+      onHide={onHide}
+      dialogAs="div"
+      dialogClassName="custom-modal w-75 bg-white m-5 mx-auto rounded p-4"
+    >
+      {children}
+    </Modal>
+  );
+}
+
+export default CustomModal;
